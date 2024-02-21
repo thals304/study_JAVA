228fa3cb76366943f89a2b92d35c8e928fe52917
somin <somin@WIN-IJD1DU6VTU5>
2024-02-21 21:02:42 +0900

    24.02.21

--- 04_java_string/src/step4_01/string/StringEx15_연습1.java
+++ 04_java_string/src/step4_01/string/StringEx15_연습1.java
@@ -2,11 +2,13 @@ package step4_01.string;
 
 import java.util.Scanner;
 
-// 24.02.
+// 24.02.20 time 16:03-16:17
 
 /*
  * today 소감문
- * 
+ * 오랜만에 가벼운 걸 풀어보고 싶어 왔는데 수정할 부분이 쫌 있던 문제였다.
+ * 입력한 문자열을 문자로 쪼갰기 때문에 숫자가 0-9까지 있는 문자 배열로 만들어줘야 했다.
+ * 이중 for문 안쪽 for문에서 j를 i로 쓰는 치명적인 실수를 요즘 많이 하고 있는데 정신차려야겠다.
  * */
 /*
  * # 문자열 속 숫자검사
@@ -27,7 +29,29 @@ public class StringEx15_연습1 {
 		
 		System.out.print("입력 : ");
 		String text = scan.next();
-
+		
+		char[] num = {'0','1','2','3','4','5','6','7','8','9'};
+		int numCnt = 0;
+		// text 한 글자와 num 배열 요소 전체를 비교하고 싶었는데 숫자를 입력하면 문제 발생
+		for (int i = 0; i < text.length(); i++) {
+			for (int j = 0; j < num.length; j++) {
+				if (text.charAt(i) == num[j]) {
+					numCnt++;
+					break; // break를 넣도 되고 안넣어도 됨
+				}
+			}
+		}
+		if (numCnt == 0) {
+			System.out.println(text + "는 문자만 있다");
+		}
+		else if  (numCnt == text.length()){
+				System.out.println(text + "는 숫자만 있다");
+			}
+		else {
+				System.out.println(text + "는 문자와 숫자가 섞여있다");
+			}
+		
+		scan.close();
 	}
 
 }
--- 04_java_string/src/step4_01/string/StringEx16_연습1.java
+++ 04_java_string/src/step4_01/string/StringEx16_연습1.java
@@ -1,7 +1,14 @@
 package step4_01.string;
 
 import java.util.Scanner;
-
+// 24.02.21 time 14:33-14:43
+/*
+ * today 소감문
+ * 일단 난 이 문제를 도대체 어떻게 풀고 어떻게 건들여야 할지 몰랐다. 
+ * 아예 못풀지도 모르겠다고 생각했다.
+ * 근데 풀었다!!!! 내가 생각한 논리가 맞았던 것이다.
+ * 굉장히 기분이 좋고 너무 신기할 따름이다.
+ * */
 /*
  * # 단어 검색
  * 
@@ -21,6 +28,24 @@ public class StringEx16_연습1 {
 		
 		System.out.print("검색할 단어를 입력하세요 : ");
 		String word = scan.next();
+		
+		// System.out.println(text.length()); 띄어쓰기도 길이로 포함
+		
+		// 연속적으로 단어가 있는지 확인을 어떻게 하지?
+		// text를 한글자씩 word의 첫 글자랑 같은지 확인하고 
+		// word의 글자 길이만큼 subString으로 text 단어를 뽑아 word랑 같으면 check = 1 
+		int check = -1;
+		for(int i = 0; i < text.length(); i++) {
+			if (text.charAt(i) == word.charAt(0)) {
+				if (text.substring(i, i + word.length()).equals(word)) { // subString(1,4) : 인덱스 1부터 3까지 출력 
+					check = 1;
+					break;
+				}
+			}
+		}
+		
+		if (check == 1) System.out.println("true");
+		else if (check == -1) System.out.println("false");
 
 	}
 
