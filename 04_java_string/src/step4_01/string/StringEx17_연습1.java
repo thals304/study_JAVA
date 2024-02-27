d90d6060cb0152a93bcb8a62222712c6002d26c5
somin <somin@WIN-IJD1DU6VTU5>
2024-02-27 22:51:07 +0900

    commit

--- 04_java_string/src/step4_01/string/StringEx17_연습1.java
+++ 04_java_string/src/step4_01/string/StringEx17_연습1.java
@@ -1,7 +1,18 @@
 package step4_01.string;
 
+import java.util.Arrays;
 import java.util.Scanner;
-
+// 24.02.27 time 14:10-14:36
+/*
+ * today 소감문
+ * 16 예제의 응용인거 같은데 진짜 어려운 것 같다.
+ * 일단 고민할 부분들이 코드를 작성하면서 계속 생기는데 그 고민에 대해 대응을 바로 못하니
+ * 고민만 쌓이고 해결하지도 못한채로 그대로 놔두게 되는 것 같다.
+ * 정답예시를 보고 내가 써둔 부분까지 비교해봤는데 얼추 비슷했던 것 같다.
+ * 근데 거기에서 한 발자국 더 생각하지 못한 것 같아서 누가 코드를 잘 짜는지는 이 부분에서 갈리는 것 같아
+ * 많이 연습해봐야겠다는 생각밖에 안들었다. 
+ * 
+ * */
 /*
  * # 단어 교체하기(replace)
  * 
@@ -15,7 +26,6 @@ import java.util.Scanner;
  * 		Time is too short.
  */
 
-
 public class StringEx17_연습1 {
 
 	public static void main(String[] args) {
@@ -24,10 +34,56 @@ public class StringEx17_연습1 {
 
 		String text = "Life is too short.";
 		System.out.println(text);
-		
+
 		System.out.print("변경하고 싶은 단어를 입력하세요 : ");
 		String word = scan.nextLine();
 		
+		// 일단은 변경하고 싶은 단어가 문장 안에 있는지 예외처리
+		int wordSize = word.length();
+	
+		int delIdx  = -1;
+		for (int i = 0; i < text.length(); i++) {
+			if (text.charAt(i) == word.charAt(0)) {
+				if (text.substring(i, i + wordSize).equals(word)) { // text 인덱스 범위는 i부터  i + wordSize - 1
+					delIdx = i;
+					break;
+				}
+			}
+		}
+		
+		if (delIdx == -1) {
+			System.out.println("변경하고 싶은 단어를 다시 확인해 주세요");
+		}
+		else {
+			System.out.print("바꿀 단어 입력 : ");
+			String changeWord = scan.next();
+			// 근데 배열이 아니라 문자열이라서 문자열 수정? 삽입?을 어떻게 하지
+			// 바꿀 단어가 wordSize의 길이와 다르다면?
+			// replaceAll(,); , deleteCharAt(); 사용 가능? 사용하지 않고 풀고 싶음
+			char[] temp = new char[text.length()];
+			int j = 0;
+			for (int i = 0; i < text.length(); i++) {
+				temp[j] = text.charAt(i);
+			}
+			// 바꿀 부분의 인덱스를 아는 걸 이용해서 바꿀 부분 제외하고 front, back으로 나누어 저장 
+			String front = "";
+			for (int i = 0; i < delIdx; i++) {
+				front += text.charAt(i);
+			}
+			
+			String back = "";
+			for (int i = delIdx + wordSize; i < text.length(); i++) {
+				back += text.charAt(i);
+			}
+			
+			String result = "";
+			result = front + changeWord + back;
+			
+			System.out.println(result);
+			
+		}
+		
+		scan.close();
 	}
 
 }
--- 04_java_string/src/step4_01/string/StringEx18_연습1.java
+++ 04_java_string/src/step4_01/string/StringEx18_연습1.java
@@ -1,14 +1,21 @@
 package step4_01.string;
 
+import java.util.Random;
 import java.util.Scanner;
-
+// 24.02.27 time 22:14-22:48
+/*
+ * today 소감문
+ * 문제에서 주어진 조건은 별로 없는데 구현하는게 쉽지 않은 것 같다.
+ * 내일 좀 더 시간을 투자해 고민해 보고 정 안되면 정답예시를 보면서 충분히 이해한 후 
+ * 나만의 방법으로 바꾸는 시도를 해봐야겠다.
+ * */
 /*
 
 	# 영어단어 맞추기  
 	
 	1) 영어단어가 전부 * 로 표시된다. 
 	2) 영어단어를 입력받고 틀릴때마다 랜덤으로 한글자씩 벗겨진다. (점수는 100점 만점이며 한번 틀릴때마다 5점씩 감점)
-	3) 만약에 같은 철자가 여러개면 한번에 벗겨진다. 
+	3) 만약에 같은 철자가 여러 개면 한번에 벗겨진다. 
 	4) 전부 벗겨지거나 맞추면 종료하고 점수를 화면에 출력한다.
 
 */
@@ -17,21 +24,56 @@ public class StringEx18_연습1 {
 	public static void main(String[] args) {
 		
 		Scanner scan = new Scanner(System.in);
+		Random ran = new Random();
 		int score = 100;
 		String word = "performance";
 		String meaning = "공연";
-		
+		System.out.println("뜻 : " + meaning);
+		System.out.print("문제 : ");
+		for (int i = 0; i < word.length(); i++) {	
+			System.out.print("*");
+			
+		}
 		while (true) {
 			
-			System.out.println("뜻 : " + meaning);
-			System.out.print("문제 : ");
-			for (int i = 0; i < word.length(); i++) {
-				System.out.print("*");
-				
-			}
+			
 			System.out.println();
 			System.out.print("영어단어를 입력하세요 >>> ");
 			String answer = scan.next();
+			int cntStar = word.length();
+			String temp = word;
+			if (answer.length() != word.length()) {
+				System.out.println("글자수가 다릅니다 다시 입력하세요");
+				continue;
+			}
+			
+			if (word.equals(answer)) {
+				System.out.println("정답! 게임종료");
+				break;
+			}
+			else if (cntStar == 0) { // *가 전부 벗겨지면을 어떻게 표현하지? 개수가 0이 되는 조건식을 걸면 될 거 같은데 
+				System.out.println("*가 전부 벗겨져서 게임종료");
+				break;
+			}
+			else { // 정답이 아닐 경우 랜덤으로 한 글자씩 벗겨짐
+				score -= 5;
+				System.out.println("뜻 : " + meaning);
+				System.out.print("문제 : ");
+				// 근데 한 글자씩 벗겨진 것이 누적이 되도록 어떻게 하지?
+				int ranIdx = ran.nextInt(word.length());	// [0-10]
+				for (int i = 0; i < word.length(); i++) {
+					if (temp.charAt(ranIdx) == temp.charAt(i)) {	// 글자들 중 랜덤 인덱스에 해당하는 글자와 같은 경우
+						System.out.print(temp.charAt(i));
+						temp += temp.charAt(i);
+						cntStar --;
+					}
+					else {
+						System.out.print("*"); 
+						temp += "*";
+					}
+				}
+			}
+			
 			
 		}
 
